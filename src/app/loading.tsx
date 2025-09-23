e7061c861c8304fb2bd8dfbc1dbaeee8b20920c3
kijima-riku <riku.bulls.0116@icloud.com>
2025-09-23 18:19:34 +0900

    style: adjust overall UI

---
+++ src/app/loading.tsx
@@ -0,0 +1,20 @@
+import { LoadingSkeleton } from "@/components/loading-skeleton"
+
+export default function Loading() {
+    return (
+        <div className="min-h-screen bg-background">
+            <div className="max-w-6xl mx-auto px-6 py-12">
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
+                    <div className="lg:col-span-1">
+                        <div className="sticky top-12">
+                            <LoadingSkeleton />
+                        </div>
+                    </div>
+                    <div className="lg:col-span-2">
+                        <LoadingSkeleton />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
